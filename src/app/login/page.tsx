acf5b0e596201eff0c9fa50696bc59ed82667d20
Somkumar <somkumarav@gmail.com>
2023-10-29 14:21:57 +0530

    changed some unwanted fuction for production testing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,8 @@
 import { Button } from "../_components/ui/Button";
 import { Input } from "../_components/ui/Input";
 import { signIn } from "next-auth/react";
-import { api } from "../../trpc/server";
 
 const LoginPage = () => {
-  const handleRegister = () => {};
   console.log(process.env.NEXTAUTH_URL);
   return (
     <div className="bg-skin-base min-h-screen">
@@ -15,9 +13,7 @@ const LoginPage = () => {
           Login
           <Input name="email" className="mb-2" />
           <Input name="password" className="mb-2" />
-          <Button onClick={handleRegister} className="mb-2">
-            Sign In
-          </Button>
+          <Button className="mb-2">Sign In</Button>
           <Button onClick={() => signIn("google")}>Sign In with Google</Button>
         </div>
       </div>
