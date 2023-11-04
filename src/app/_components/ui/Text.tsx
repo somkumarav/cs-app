1d73dc6d040803bbaff84b79817d059f7188af6e
Somkumar <somkumarav@gmail.com>
2023-11-04 15:03:42 +0530

    added fonts and Text component

---
+++ src/app/_components/ui/Text.tsx
@@ -0,0 +1,69 @@
+import { cva } from "class-variance-authority";
+import { cn } from "../../../utils/cn";
+
+export const TextVariants = cva("", {
+  variants: {
+    varients: {
+      default: "text-skin-primaryColor",
+      secondary: "text-skin-secondaryColor",
+    },
+    textSize: {
+      paragraph: "text",
+      heading: "text-xl",
+      timer: "text-6xl ",
+      scramble: "text-2xl tracking-wider text-center",
+    },
+  },
+  defaultVariants: {
+    varients: "default",
+    textSize: "paragraph",
+  },
+});
+type TextVariants = typeof TextVariants;
+
+interface IProps {
+  children: React.ReactNode;
+  className?: string;
+  textSize?: "paragraph" | "heading" | "timer" | "scramble";
+  varient?: "default" | "secondary";
+}
+
+export const Text = ({ children, className, textSize, varient }: IProps) => {
+  return (
+    <div>
+      {textSize === "heading" ? (
+        <h1
+          className={cn(
+            TextVariants({ varients: varient, textSize, className }),
+          )}
+        >
+          {children}
+        </h1>
+      ) : textSize === "scramble" ? (
+        <h1
+          className={cn(
+            TextVariants({ varients: varient, textSize, className }),
+          )}
+        >
+          {children}
+        </h1>
+      ) : textSize === "timer" ? (
+        <h1
+          className={cn(
+            TextVariants({ varients: varient, textSize, className }),
+          )}
+        >
+          {children}
+        </h1>
+      ) : (
+        <p
+          className={cn(
+            TextVariants({ varients: varient, textSize, className }),
+          )}
+        >
+          {children}
+        </p>
+      )}
+    </div>
+  );
+};
