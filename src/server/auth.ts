b9ded3dbc38e41b35394eab29d167876e349d008
Somkumar <somkumarav@gmail.com>
2023-12-03 20:37:40 +0530

    fixed auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -23,27 +23,39 @@ declare module "next-auth" {
       // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
+    session: ({ session, token }) => ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        id: token.uid,
       },
     }),
+    redirect: ({ url, baseUrl }) => {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+    jwt: ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 1 day
+  },
+  pages: {
+    signIn: "/signin",
   },
   adapter: PrismaAdapter(db),
   providers: [
@@ -64,8 +76,103 @@ export const authOptions: NextAuthOptions = {
 };
 
 /**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+// export const authOptions: NextAuthOptions = {
+//   callbacks: {
+//     session: ({ session, user }) => ({
+//       ...session,
+//       user: {
+//         ...session.user,
+//         id: user.id,
+//       },
+//     }),
+//   },
+//   adapter: PrismaAdapter(db),
+//   pages:{
+//     signIn: "/signin",
+//   },
+//   providers: [
+//     GoogleProvider({
+//       clientId: env.GOOGLE_CLIENT_ID,
+//       clientSecret: env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     /**
+//      * ...add more providers here.
+//      *
+//      * Most other providers require a bit more work than the Discord provider. For example, the
+//      * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+//      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+//      *
+//      * @see https://next-auth.js.org/providers/github
+//      */
+//   ],
+// };
+
+/**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = () => getServerSession(authOptions);
+
+// export const authOptions: NextAuthOptions = {
+//   callbacks: {
+//     session({ session, user, token }) {
+//       console.log(user);
+//       console.log("token--->", token);
+//       if (session.user) {
+//         session.user.id = user.id;
+//       }
+//       return session;
+//     },
+//     redirect({ url, baseUrl }) {
+//       // Allows relative callback URLs
+//       if (url.startsWith("/")) return `${baseUrl}${url}`;
+//       // Allows callback URLs on the same origin
+//       else if (new URL(url).origin === baseUrl) return url;
+//       return baseUrl;
+//     },
+//     jwt({ user, token }) {
+//       if (user) {
+//         token.uid = user.id;
+//       }
+//       return token;
+//     },
+//   },
+//   // callbacks: {
+//   //   session: ({ session, user }) => ({
+//   //     ...session,
+//   //     user: {
+//   //       ...session.user,
+//   //       id: user.id,
+//   //     },
+//   //   }),
+//   // },
+//   providers: [
+//     GoogleProvider({
+//       clientId: env.GOOGLE_CLIENT_ID!,
+//       clientSecret: env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//     /**
+//      * ...add more providers here.
+//      *
+//      * Most other providers require a bit more work than the Discord provider. For example, the
+//      * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+//      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+//      *
+//      * @see https://next-auth.js.org/providers/github
+//      */
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+//   session: {
+//     strategy: "jwt",
+//     maxAge: 30 * 24 * 60 * 60, // 30 days
+//     updateAge: 24 * 60 * 60, // 1 day
+//   },
+//   pages: {
+//     signIn: "/signin",
+//   },
+// };
